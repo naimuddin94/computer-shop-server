a39aac45c012868b5c1de5b5f2a7bcc1c4d7478c
naimuddin94 <developernaimuddin@gmail.com>
2023-12-05 22:18:36 +0600

    database connection

--- index.js
+++ index.js
@@ -2,20 +2,25 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const app = express();
-const productRoutes = require("./routes/productRoutes")
-
+const productRoutes = require("./routes/productRoutes");
+const connectDB = require("./database/connectDB");
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/products", productRoutes)
+app.use("/products", productRoutes);
 
-app.get("/", (req, res) => { 
-    res.send("server is running")
-})
-
-app.listen(port, () => {
-    console.log(`server listening on port ${port}`);
+app.get("/", (req, res) => {
+  res.send("server is running");
 });
+
+const main = async (req, res) => { 
+    await connectDB()
+    app.listen(port, () => {
+      console.log(`server listening on port ${port}`);
+    });
+}
+
+main()
