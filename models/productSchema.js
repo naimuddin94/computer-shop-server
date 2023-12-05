8d682b57c8cf19d4e338798d4ae556320b0db5e2
naimuddin94 <developernaimuddin@gmail.com>
2023-12-05 22:39:10 +0600

    product schema create

---
+++ models/productSchema.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  cover_image: {
+    type: String,
+    required: true,
+  },
+  images: {
+    type: [String],
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  price: {
+    typeof: Number,
+    required: true,
+  },
+  features: {
+    type: [String],
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  publisher: {
+    name: String,
+    email: String,
+  },
+  created_at: {
+    typeof: String,
+    default: Date.now(),
+  },
+});
