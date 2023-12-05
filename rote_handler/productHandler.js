465f96b0c166bc6c1e054487309e18124bf6357c
naimuddin94 <developernaimuddin@gmail.com>
2023-12-05 23:01:44 +0600

    route create get all product and create new product

--- rote_handler/productHandler.js
+++ rote_handler/productHandler.js
@@ -1,5 +1,28 @@
+const mongoose = require("mongoose");
+const productSchema = require("../models/productSchema");
+const handleError = require("../lib/errorHandler");
+const Product = mongoose.model("product", productSchema);
+
+// get all products
 const getAllProducts = async (res, req) => {
-  res.send("Products found successfully");
+  const query = {};
+  try {
+    const products = await Product.find(query);
+    res.status(200).send(products);
+  } catch (error) {
+    handleError(res, error);
+  }
 };
 
-module.exports = { getAllProducts };
+// create a new product
+const createProduct = async (req, res) => {
+  const product = req.body;
+  try {
+    await Product.create(product);
+    res.status(201).json({ message: "Product added successfully" });
+  } catch (error) {
+    handleError(res, error);
+  }
+};
+
+module.exports = { getAllProducts, createProduct};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,7 +1,8 @@
-const { getAllProducts } = require("../rote_handler/productHandler");
+const { getAllProducts, createProduct } = require("../rote_handler/productHandler");
 
 const router = require("express").Router();
 
 router.get("/", getAllProducts);
+router.post("/create-product", createProduct);
 
 module.exports = router;
